b6444face8bf096b6d4cbd3b257cca0ce37de5a4
LeeHakYoung <dlgkrdud3@gmail.com>
2019-05-25 17:30:46 +0900

    ## 프리팹 다시 제작 ## - 곰 순회경로 제작. - 충돌처리 제작. - 애니메이터 제작중.

--- Assets/Scripts/HakYoung/AnimalMoveAgent.cs
+++ Assets/Scripts/HakYoung/AnimalMoveAgent.cs
@@ -1,7 +1,10 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
+//RequireComponent를 사용하는 스크립트를 추가하면, 요구되는 컴포너트가 자동으로 해당 게임오브젝트에 추가됩니다.
+[RequireComponent(typeof(NavMeshAgent))]
 public class AnimalMoveAgent : MonoBehaviour
 {
 
@@ -9,9 +12,17 @@ public class AnimalMoveAgent : MonoBehaviour
     public List<Transform> wayPoint;
     //다음 인덱스로 넘어가기 위한 인트형 변수.
     public int nextIdx;
+    //컴포넌트 저장 변수.
+    private NavMeshAgent agent;
+
 
     void Start()
     {
+        //NavMeshAgent 컴포넌트를 추출후 변수에 저장.
+        agent = GetComponent<NavMeshAgent>();
+        // 목적지에 가까워질수록 속도를 줄이는 옵션을 비활성화.
+        agent.autoBraking = false;
+
         //하이러키 뷰의 WayPointGroup 게임오브젝트를 추출한다.
         var group = GameObject.Find("BearWayPointGroup");
         //포인트가 있다면.
@@ -20,6 +31,29 @@ public class AnimalMoveAgent : MonoBehaviour
 
             group.GetComponentsInChildren<Transform>(wayPoint);
             wayPoint.RemoveAt(0);
+        }
+        MoveWayPoint();
+    }
+
+    void MoveWayPoint()
+    {
+        //최단거리 경로 계산이 끝나지 않았으면 다음을 수행하지 않음.
+        if (agent.isPathStale) return;
+
+        //다음목적지를 wayPoint 배열에서 추출한 위치로 다음 목적지를 지정.
+        agent.destination = wayPoint[nextIdx].position;
+
+        //내비게이션 기능을 활성화해서 이동을 삭제함.
+        agent.isStopped = false;
+    }
+
+    void Update()
+    {
+        //NavMeshAgent가 이동하고 있고 목적지에 도착했는지 여부를 계산.
+        if(agent.velocity.sqrMagnitude >= 0.2f * 0.2f && agent.remainingDistance <= 0.5f)
+        {
+            nextIdx = ++nextIdx % wayPoint.Count;
+            MoveWayPoint();
         }
     }
 }
--- Assets/Scripts/HakYoung/GameManager.cs
+++ Assets/Scripts/HakYoung/GameManager.cs
@@ -6,14 +6,16 @@ public class GameManager : MonoBehaviour
 {
 
     public Transform[] bearPoints;          //곰 생성 포인트를 담아놓기 위한 Transfrom배열.
-
-    public Transform[] rabbitPoints;         //토끼 생성 포인트를 담아놓기 위한 Transfrom배열.
-
+    public Transform[] rabbitPoints;        //토끼 생성 포인트를 담아놓기 위한 Transfrom배열.
+    public Transform[] cougarPoints;        //쿠거 생성 포인트를 담아놓기 위한 Transfrom배열.
 
     [SerializeField]                        //생성해놓지 않은 프리팹의 베어를 가져오기 위한 시리얼라이즈 필드
     public GameObject[] bears;              //베어를 담아놓기 위한 배열.
     [SerializeField]
     public GameObject[] rabbits;             //토끼를 담아놓기 위한 배열.
+    [SerializeField]
+    public GameObject[] cougars;            //쿠거를 담아놓기 위한 배열.
+
 
     public float createTime = 4.0f;         //생성속도 4초당 1마리
     public int maxAnimal = 10;              //최대 생성될 수 있는 마릿수
@@ -22,9 +24,8 @@ public class GameManager : MonoBehaviour
     void Start()
     {
         bearPoints = GameObject.Find("BearSpawnPointGroup").GetComponentsInChildren<Transform>();       //생성포인트를 불러와서 배열에 담습니다.
-
         rabbitPoints = GameObject.Find("RabbitSpawnPointGroup").GetComponentsInChildren<Transform>();
-
+        cougarPoints = GameObject.Find("CougarSpawnPointGroup").GetComponentsInChildren<Transform>();
 
         if (bearPoints.Length > 0)                  //생성포인트가 있다면
         {
@@ -34,6 +35,11 @@ public class GameManager : MonoBehaviour
         if(rabbitPoints.Length > 0)
         {
             StartCoroutine(this.CreateRabbit());
+        }
+
+        if(cougarPoints.Length > 0)
+        {
+            StartCoroutine(this.CreateCougar());
         }
     }
 
@@ -83,6 +89,33 @@ public class GameManager : MonoBehaviour
                 int rabbitsIdx = Random.Range(0, rabbits.Length - 1);
                 //위의 정보들로 생성해라.
                 Instantiate(rabbits[rabbitsIdx], rabbitPoints[idx].position, rabbitPoints[idx].rotation);
+            }
+            //없다면 다음생성시간을 없에서 생성을 막아라.
+            else
+                yield return null;
+        }
+    }
+
+    IEnumerator CreateCougar()
+    {
+        //죽지 않았다면
+        while (!isGameOver)
+        {
+            //현재 몇마리가 있는지 체크하고
+            int cougarCount = (int)GameObject.FindGameObjectsWithTag("Cougar").Length;
+
+            //맥스치보다 작다면
+            if (cougarCount < maxAnimal)
+            {
+                // 
+                yield return new WaitForSeconds(createTime);
+
+                //생성포인트 기존에 깔아놓은 장소중에서 랜덤으로 생성시키기 위한 인덱스값.
+                int idx = Random.Range(1, cougarPoints.Length);
+                //토끼도 종류별로 랜덤으로 생성시키기 위한 인덱스값
+                int cougarIdx = Random.Range(0, cougars.Length - 1);
+                //위의 정보들로 생성해라.
+                Instantiate(cougars[cougarIdx], cougarPoints[idx].position, cougarPoints[idx].rotation);
             }
             //없다면 다음생성시간을 없에서 생성을 막아라.
             else
